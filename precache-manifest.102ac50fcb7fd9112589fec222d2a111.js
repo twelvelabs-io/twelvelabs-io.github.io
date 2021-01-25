6a07a493eb8ccf1476f32d11d8c99bdd48089496
haramjo <haram@yhat-ai.com>
2021-01-25 23:14:38 +0900

    Updates

---
+++ precache-manifest.102ac50fcb7fd9112589fec222d2a111.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21869456ea47d7ea479ccb6a6ce10a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e51160a4cce0cfa880",
+    "url": "/static/css/main.668ffe3d.chunk.css"
+  },
+  {
+    "revision": "d225bc182fd813f3853a",
+    "url": "/static/js/2.3507a3f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3507a3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e51160a4cce0cfa880",
+    "url": "/static/js/main.c8f8f866.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "f8c97b9a10e7d0d54a09228981e8737b",
+    "url": "/static/media/aiden.f8c97b9a.png"
+  },
+  {
+    "revision": "35577ddc9ed92710b0dc1046d2484263",
+    "url": "/static/media/dave.35577ddc.png"
+  },
+  {
+    "revision": "c38d3017f57c44dc6640303b37ef65ac",
+    "url": "/static/media/hallazzang.c38d3017.png"
+  },
+  {
+    "revision": "1d235d008551a5b102589934a19d67a0",
+    "url": "/static/media/haram.1d235d00.png"
+  },
+  {
+    "revision": "811fbaa90b680e19c4374d43577e17bd",
+    "url": "/static/media/jae.811fbaa9.png"
+  },
+  {
+    "revision": "3fd560fde067dbc68bada38f8b3e7918",
+    "url": "/static/media/ray.3fd560fd.png"
+  },
+  {
+    "revision": "1208f452faaac9b88e37598784be09fe",
+    "url": "/static/media/right-arrow-button.1208f452.svg"
+  },
+  {
+    "revision": "2f81ea785a9c074a31f7281004e5298a",
+    "url": "/static/media/right-arrow.2f81ea78.svg"
+  },
+  {
+    "revision": "4ec3924c4bd437467d9ef58d00286228",
+    "url": "/static/media/sj.4ec3924c.png"
+  },
+  {
+    "revision": "41e2a52edc993f0ac371ba2e7c39a7b8",
+    "url": "/static/media/soyoung.41e2a52e.png"
+  },
+  {
+    "revision": "dca20a6ddc302b31012821a024948de7",
+    "url": "/static/media/theyoon.dca20a6d.png"
+  },
+  {
+    "revision": "36329431ada97e36ef04f36e3f239e88",
+    "url": "/static/media/zaghetto.36329431.pcd"
+  }
+]);
